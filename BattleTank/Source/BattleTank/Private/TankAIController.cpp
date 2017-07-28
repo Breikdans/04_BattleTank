536c2ebbcaeb57d4d08a7981aeae46af0f4266ea
Breikdans <los.chichos.maiden@gmail.com>
2017-07-28 21:36:08 +0200

    TankAIController detecta el PlayerTank
    Signed-off-by: Breikdans <los.chichos.maiden@gmail.com>

--- BattleTank/Source/BattleTank/Private/TankAIController.cpp
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -15,6 +15,17 @@ void ATankAIController::BeginPlay()
 	{
 		UE_LOG(LogTemp, Warning, TEXT("There is NO AI Controlled Tank!!"));
 	}
+
+	auto PlayerTank = GetPlayerTank();
+	if (PlayerTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("AI Controller found Player Tank: %s"), *PlayerTank->GetName());
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("There is NO Player Tank!!"));
+	}
+
 }
 
 ATank* ATankAIController::GetControlledTank() const
@@ -22,3 +33,7 @@ ATank* ATankAIController::GetControlledTank() const
 	return Cast<ATank>(GetPawn());
 }
 
+ATank* ATankAIController::GetPlayerTank() const
+{
+	return Cast<ATank>(GetWorld()->GetFirstPlayerController()->GetPawn());
+}
